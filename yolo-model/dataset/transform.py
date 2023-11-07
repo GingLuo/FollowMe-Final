3178afb2bbb695cc5629f040ea536fa02494c55d
GingLuo <yichuanl@andrew.cmu.edu>
2023-11-07 01:19:21 -0500

    Preprocess training set images

--- yolo-model/dataset/transform.py
+++ yolo-model/dataset/transform.py
@@ -1,57 +1,64 @@
-from PIL import Image, ImageDraw
 import os
+from PIL import Image, ImageDraw, ImageFont
+
 
 if __name__ == '__main__':
-    # image_directory = "archive/train/images/"
-    # for file in os.listdir(image_directory): 
-    #     if not file.endswith(".jpg"): 
-    #         img = Image.open(image_directory+file).convert('RGB')
-    #         file_name, file_ext = os.path.splitext(file)
-    #         img.save(f'{image_directory}{file_name}.jpg')
-    # print("Finished")
-    # input()
+    directory = "labels"
+    data = [0] * 10
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f):
+            image_label = open(f, "r")
+        # with open("./labels/"+filename, 'a') as file:
+        #     for line in image_label:
+        #         attributes = line.split(" ")
+        #         class_label = int(attributes[0])
+        #         # [121, 2, 0, 0, 52, 58, 25, 2, 10, 7]
+        #         if class_label == 0 or class_label == 1:
+        #             new_label = "80"
+        #         elif class_label == 4:
+        #             new_label = "81"
+        #         elif class_label == 5:
+        #             new_label = "56"
+        #         elif class_label == 6:
+        #             new_label == "82"
+        #         elif class_label == 7:
+        #             new_label == "82"
+        #         elif class_label == 8:
+        #             new_label = "80"
+        #         elif class_label == 9:
+        #             new_label = "12"
+        #         else:
+        #             print(filename)
+        #         attributes[0] = new_label
+        #         new_line = " ".join(attributes)
+        #     file.write(new_line)
 
-    with open('label_dataold.txt', 'a') as file:
+        path = filename[:-4] + ".jpg"
+        real_path = os.path.join("images", path)
+        img = Image.open(real_path).convert('RGB')
+        # print(real_path)
 
-        # directory = "archive/train/labels"
-        # image_directory = "archive/train/images"
-        directory = "dataold"
-        count = 0
-        for filename in os.listdir(directory):
-            if filename.endswith(".jpg"):
-                continue
-            f = os.path.join(directory, filename)
-            
-            if os.path.isfile(f):
-                image_label = open(f, "r")
+        image_width = img.width
+        image_height = img.height
+        for line in open(f, "r"):
+            attributes = line.split(" ")
+            class_label = int(attributes[0])
+            x_center = float(attributes[1]) * image_width
+            y_center = float(attributes[2]) * image_height
+            w = float(attributes[3]) * image_width
+            h = float(attributes[4]) * image_height
+            x1 = x_center - w/2
+            y1 = y_center - h/2
 
-                path = filename[:-4] + ".jpg"
-                real_path = os.path.join(directory, path)
-                file.write(f"# {path}\n")
-                img = Image.open(real_path).convert('RGBA')
-                print(real_path)
+            curr_x = (x1, x1 + w, x1 + w, x1, x1)
+            curr_y = (y1, y1, y1 + h, y1 + h, y1)
+            draw = ImageDraw.Draw(img)
+            draw.line(list(zip(curr_x,curr_y)), fill="green", width=2) 
+            font = ImageFont.load_default()
+            draw.text((x1,y1), f"Class={class_label}", fill="black", anchor="ms", font=font)
+        img.save(f"./visualizations/{path}")
 
-                image_width = img.width
-                image_height = img.height
-
-                for line in image_label:
-                    attributes = line.split(" ")
-                    x_center = float(attributes[1]) * image_width
-                    y_center = float(attributes[2]) * image_height
-                    w = float(attributes[3]) * image_width
-                    h = float(attributes[4]) * image_height
-                    x1 = x_center - w/2
-                    y1 = y_center - h/2
-                    class_label = int(attributes[0])+1
-                    file.write(f"{class_label} {x1} {y1} {w} {h}\n")
-
-                    curr_x = (x1, x1 + w, x1 + w, x1, x1)
-                    curr_y = (y1, y1, y1 + h, y1 + h, y1)
-                    draw = ImageDraw.Draw(img)
-                    draw.line(list(zip(curr_x,curr_y)), fill="green", width=2) 
-                # img.show()
-                # input()
-                count += 1
-        print(f"Finished {count} images in directory {directory}")
+    print(data)
 
 
